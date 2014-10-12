106ad0dc5dbf7deb3e49f8e981303cd3948c708e
doru <doru@DNotebook>
2014-10-12 14:39:01 +0300

    last update

--- PizzaOrder/src/com/comandapizza/pizzaorder/PizzaPickerActivity.java
+++ PizzaOrder/src/com/comandapizza/pizzaorder/PizzaPickerActivity.java
@@ -1,13 +1,32 @@
 package com.comandapizza.pizzaorder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PizzaPickerActivity extends Activity {
 
+	Button BPizzaQuatroFormaggi;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_picker_pizza);
+		BPizzaQuatroFormaggi = (Button) findViewById(R.id.IDButtonQuatroFormaggi);
+		BPizzaQuatroFormaggi.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent goToPizzaOptions = new Intent(PizzaPickerActivity.this,
+						PizzaOptionsActivity.class);
+				// goToPizzaOptions.
+				startActivity(goToPizzaOptions);
+
+			}
+		});
+
 	}
 }
